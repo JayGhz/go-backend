d8220fcd4661aa540860ac591fb7399952b3b774
JayGhz <jaykansas7@gmail.com>
2025-01-09 22:44:05 -0500

    Refactor healthcheck handler function name for consistency

--- cmd/app/healthcheck.go
+++ cmd/app/healthcheck.go
@@ -2,7 +2,7 @@ package app
 
 import "net/http"
 
-func (app *Application) HealthcheckHandler(w http.ResponseWriter, r *http.Request) { // Cambiado de 'healthcheckHandler' a 'HealthcheckHandler'
+func (app *Application) HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"alive": true}`))
